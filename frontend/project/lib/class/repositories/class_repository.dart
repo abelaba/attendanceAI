b018328446dd344c9a1c6f96ba17641b25bf253a
abelaba <se.abel.abate@gmail.com>
2022-01-16 00:28:45 +0300

    Implemented class data provider and repository

---
+++ frontend/project/lib/class/repositories/class_repository.dart
@@ -0,0 +1,24 @@
+import 'package:project/class/models/ClassName.dart';
+import 'package:project/class/data-provider/class_api_client.dart';
+
+class ClassRepostiry {
+  final ClassApiClient classApiClient;
+
+  ClassRepostiry({required this.classApiClient});
+
+  Future<ClassName> getClassDetail(int id) async {
+    return await classApiClient.getClassDetail(id);
+  }
+
+  Future<List<ClassName>> getClasses(String name, String password) async {
+    return await classApiClient.getClasses(name, password);
+  }
+
+  Future<void> addStudent(int classId, int studentId) async {
+    return await classApiClient.addStudent(classId, studentId);
+  }
+
+  Future<void> deleteClass(int id) async {
+    return await classApiClient.deleteClass(id);
+  }
+}
