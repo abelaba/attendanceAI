94395b89dd2724495a6d66679c84b753c5f31379
abelaba <se.abel.abate@gmail.com>
2022-02-03 16:01:18 +0300

    Attendance model, state and event implemented

---
+++ frontend/project/lib/attendance/bloc/attendance_state.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+import 'package:project/attendance/model/attendance_model.dart';
+
+abstract class AttendanceState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class AttendanceTakenEmpty extends AttendanceState {}
+
+class AttendnanceTaken extends AttendanceState {
+  final Attendance attendances;
+
+  AttendnanceTaken({required this.attendances});
+}
+
+class AttendanceTakenLoading extends AttendanceState {}
+
+class AttendanceTakenError extends AttendanceState {}
