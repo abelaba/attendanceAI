5b621a06e11595457b7889f94fca437973c45205
abelaba <se.abel.abate@gmail.com>
2022-03-03 04:49:15 +0300

    Home screen and user settings pages finished

---
+++ frontend/project/lib/presentation/presentation.dart
@@ -0,0 +1,7 @@
+export 'LoginPage.dart';
+export 'HomePage.dart';
+export 'ShowClass.dart';
+export 'SignUpPage.dart';
+export 'ShowUserSettings.dart';
+export 'TakeAttendance.dart';
+export 'ShowAllAttendance.dart';
