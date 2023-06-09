e25c67aa38ae2f61e92180f565bb239512ee957b
abelaba <se.abel.abate@gmail.com>
2023-06-10 02:34:30 +0300

    Sign up UI modified

--- frontend/project/lib/presentation/SignUpPage.dart
+++ frontend/project/lib/presentation/SignUpPage.dart
@@ -1,11 +1,11 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:project/Arguments/ScreenArguments.dart';
+import 'package:project/arguments/ScreenArguments.dart';
 import 'package:project/teacher/bloc/TeacherBloc.dart';
 import 'package:project/teacher/bloc/TeacherEvent.dart';
 import 'package:project/teacher/bloc/TeacherState.dart';
 import 'package:project/teacher/models/Teachers.dart';
-
 import '../../routes.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -13,26 +13,34 @@ class SignUpPage extends StatefulWidget {
   _SignUpState createState() => _SignUpState();
 }
 
-class _SignUpState extends State {
-  final namecontroller = TextEditingController();
-  final passwordcontroller = TextEditingController();
-  final _key = GlobalKey<FormState>();
+class _SignUpState extends State<SignUpPage> {
+  final nameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  Widget _Name() {
+  Widget _buildNameField() {
     return TextFormField(
-      controller: namecontroller,
-      decoration: const InputDecoration(
-          icon: Icon(Icons.verified_user), hintText: "User Name"),
+      controller: nameController,
+      decoration: InputDecoration(
+        labelText: "User Name",
+        prefixIcon: Icon(Icons.verified_user),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+      ),
       validator: _validateName,
     );
   }
 
-  Widget _password() {
+  Widget _buildPasswordField() {
     return TextFormField(
-      controller: passwordcontroller,
-      decoration: const InputDecoration(
-        icon: Icon(Icons.vpn_key),
-        hintText: "Password",
+      controller: passwordController,
+      decoration: InputDecoration(
+        labelText: "Password",
+        prefixIcon: Icon(Icons.vpn_key),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
       ),
       obscureText: true,
       validator: _validatePassword,
@@ -42,81 +50,123 @@ class _SignUpState extends State {
   String? _validateName(String? value) {
     if (value!.isEmpty) {
       return "Name can not be empty";
-    } else {
-      return null;
     }
+    return null;
   }
 
   String? _validatePassword(String? value) {
     if (value!.length < 8) {
       return "Password should be greater than 8";
-    } else {
-      return null;
     }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TeacherBloc, TeacherState>(
-      builder: (context, state) {
-        return MaterialApp(
-          home: Scaffold(
-            body:  SizedBox(
-                child: Form(
-                  child: Padding(
-                    padding: const EdgeInsets.all(80.0),
+    return Scaffold(
+      body: BlocBuilder<TeacherBloc, TeacherState>(
+        builder: (context, state) {
+          return Stack(
+            children: [
+              _buildBackgroundImage(), // Background image
+              _buildBlurEffect(), // Blur effect
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Form(
+                    key: _formKey,
                     child: Column(
-                      mainAxisAlignment:MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        _Name(),
-                        _password(),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 14, bottom: 12),
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                minimumSize: Size(234, 44)),
-                            onPressed: () => {
-                              if (_key.currentState!.validate())
-                                {
-                                  print("start"),
-                                  
-                                  if (state is OneTeacherLoaded)
-                                    {
-                                      Navigator.of(context)
-                                          .pushNamed(RouteGenerator.otherPage,arguments: new ScreenArguments(classId: 0, className: "", name: this.namecontroller.text, password: this.passwordcontroller.text)),
-                                      new CircularProgressIndicator(),
-                                    }
-                                  else
-                                    {
-                                      print("create teacher"),
-                                      BlocProvider.of<TeacherBloc>(context).add(
-                                          CreateTeacher(new Teacher(
-                                              name: namecontroller.text,
-                                              password: passwordcontroller.text,
-                                              id: 0))),
-                              
-                                    }
-                                  
-                                }
-                              
-                            },
-                            child: Text("signup"),
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
+                        SizedBox(height: 20),
+                        _buildNameField(),
+                        SizedBox(height: 20),
+                        _buildPasswordField(),
+                        SizedBox(height: 20),
                         ElevatedButton(
-                            onPressed: () => {Navigator.pop(context)},
-                            child: Text("back"))
+                          style: ElevatedButton.styleFrom(
+                            primary:
+                                Color(0xFF00AEEF), // Vibrant futuristic blue
+                            onPrimary: Colors.white,
+                            minimumSize: Size(234, 44),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            elevation: 3.0,
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              if (state is OneTeacherLoaded) {
+                                Navigator.of(context).pushNamed(
+                                  RouteGenerator.otherPage,
+                                  arguments: ScreenArguments(
+                                    classId: 0,
+                                    className: "",
+                                    name: nameController.text,
+                                    password: passwordController.text,
+                                  ),
+                                );
+                              } else {
+                                BlocProvider.of<TeacherBloc>(context).add(
+                                  CreateTeacher(
+                                    new Teacher(
+                                      name: nameController.text,
+                                      password: passwordController.text,
+                                      id: 0,
+                                    ),
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          child: Text("Sign Up"),
+                        ),
+                        SizedBox(height: 10),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: TextButton.styleFrom(
+                            primary: Color(0xFF2A2A2A), // Dark metallic gray
+                          ),
+                          child: Text("Back"),
+                        ),
                       ],
                     ),
                   ),
-                  key: _key,
                 ),
               ),
-            
-          ),
-        );
-      },
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return Positioned.fill(
+      child: Image.asset(
+        'assets/images/background.jpeg', // Replace with your background image URL
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _buildBlurEffect() {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+      child: Container(
+        color: Colors.white.withOpacity(0.5),
+      ),
     );
   }
 }
